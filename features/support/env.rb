09c7e4e8b340c0e6d10e38cdbd3d3452007138f0
mathsantosilva <matheus.santos.x4@gmail.com>
2022-08-05 00:04:50 -0300

    Concluido a montagem do ambiente para iniciar a automação

---
+++ features/support/env.rb
@@ -0,0 +1,18 @@
+# Relatorios no formato Json
+require "allure-cucumber"
+# Subir uma instancia do appium e le os capabilities
+require "appium_lib"
+# Limpar os logs e screenshots
+require "fileutils"
+
+# Limpa os logs e os screenshots
+FileUtils.rm_f(Dir.glob("logs/*.json"))
+FileUtils.rm_f(Dir.glob("logs/*.png"))
+
+
+# Carrega os Capabilities da pasta caps
+caps = Appium.load_appium_txt file: File.expand_path("caps/android.txt", __dir__), verbose: true
+# Levanta uma instancia do driver do appium passando o capabilities 
+Appium::Driver.new(caps, true)
+# Passando os metodos do appium para o cucumber
+Appium.promote_appium_methods Object
---
+++ features/support/hooks.rb
@@ -0,0 +1,31 @@
+Before do
+    ## Inicia a sessão do driver
+    driver.start_driver
+    ## aumenta o timeout para encontrar elementos
+    driver.manage.timeouts.implicit_wait = 30
+  
+  end
+  
+  After do |scenario|
+    ## Após terminar um cenario ele tira uma screenshot no formato base64
+    binary_shot = driver.screenshot_as(:base64)
+  
+    ## caminho temporario do screenshot
+    temp_shot = "logs/temp_shot.png"
+  
+    ## Transforma de binario em arquivo
+    File.open(temp_shot, "wb") do |f|
+      f.write(Base64.decode64(binary_shot).force_encoding("UTF-8"))
+    end
+  
+    ## anexa a screenshot
+    Allure.add_attachment(
+      name: "screenshot",
+      type: Allure::ContentType::PNG,
+      source: File.open(temp_shot),
+    )
+  
+    ## Finaliza a sessão do appium
+    driver.quit_driver
+  end
+  
