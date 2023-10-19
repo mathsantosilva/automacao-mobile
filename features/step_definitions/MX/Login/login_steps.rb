3a605a851aa6438f8f3f28d24076ceafb662a1c1
Matheus Santos <matheus.santos.x4@gmail.com>
2023-10-19 16:39:30 -0300

    Implementações: - Entregando correções

--- features/step_definitions/MX/Login/login_steps.rb
+++ features/step_definitions/MX/Login/login_steps.rb
@@ -42,7 +42,36 @@ Quando('clico no botão em espanhol {string}') do |botao|
   find_element(xpath: "#{caminho_senha}").send_keys('1')
   find_element(xpath: "//android.widget.TextView[@text='#{botao}']").click
   sleep 2                                                                                                                                   
-end                                                                                                                                                                                                              
+end 
+
+Quando('clicar em marcar ponto no aplicativo MX deverá receber a mensagem {string} e ficar com a quantidade de {string} marcações por enviar') do |string, string2|
+  caminho_marcar_ponto = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[4]/android.widget.ImageView'
+  caminho_message_sucess = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup/android.widget.TextView'
+  caminho_quant_marc = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]/android.view.ViewGroup[2]/android.view.ViewGroup[2]/android.widget.TextView'     
+  @login.permissao_gps
+  find_element(xpath: "#{caminho_marcar_ponto}").click
+  @login.permissao_pasta  
+  sleep 2
+  message_atual = find_element(xpath: "#{caminho_message_sucess}").text
+  expect(message_atual).to eql message_sucess
+  quant_atual = find_element(xpath: "#{caminho_quant_marc}").text
+  expect(quant_atual).to eql quantidade   
+end
+
+Então('deverá contem uma marcação {string}, clicando em sincronizar deverá informar {string} e o status de {string} no envio para o sistema MX') do |string, string2, string3|
+  caminho_marc_sinc = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.widget.ScrollView/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[6]/android.view.ViewGroup[3]/android.widget.ImageView'
+  caminho_status_marc = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ListView/android.widget.LinearLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.TextView'
+  caminho_botao_sinc = '/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.RelativeLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[3]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.widget.ImageView'
+  find_element(xpath: "#{caminho_marc_sinc}").click   
+  status_atual = find_element(xpath: "#{caminho_status_marc}").text
+  expect(status_atual).to eql status
+  find_element(xpath: "#{caminho_botao_sinc}").click
+  message_sinc_atual = find_element(id: "android:id/message").text
+  expect(message_sinc_atual).to eql message_sucess
+  find_element(id: "android:id/button2").click
+  status_atual_sinc = find_element(xpath: "#{caminho_status_marc}").text
+  expect(status_atual_sinc).to eql status_sinc 
+end
                                                                                                                                                                                                                  
 Dado('realizo o login com o email {string} e  a senha {string} master e clico em {string} no aplicativo MX') do |email, senha, botao|                                                                       
   @login.permissao_bluetooth
